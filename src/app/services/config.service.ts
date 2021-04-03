4f72ecfd49825885dc3bd3c22865b9e3a89ef307
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-03 12:34:27 +0300

    add the service logic

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  constructor() { }
+  private user!: string;
+
+  constructor(private http: HttpClient) {
+    this.user = 'Paul-Ngigi'
+  }
+
+  getUser(): Observable<any> {
+    return this.http.get("https://api.github.com/users/" + this.user + '?access_token='+ environment.searchApiKey)
+  }
+
+  getRepos(): Observable<any>{
+    return this.http.get("https://api.github.com/users/" + this.user + "/repos?access_token" + environment.searchApiKey)
+  }
+
+  profileUpdate(username: any){
+    this.user = username;
+  }
 }
