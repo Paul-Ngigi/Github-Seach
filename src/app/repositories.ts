390bc309d18f77a51b3b375d08ffcd909540b515
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-05 09:02:32 +0300

    Add a repositories class

---
+++ src/app/repositories.ts
@@ -0,0 +1,4 @@
+export class Repositories {
+    constructor(public name: string, public html_url: string, public description: string, public created_at: Date
+        , public language: string) { }
+}
