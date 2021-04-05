361af1d2225e33f81403e972117b20f71be48b0a
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-05 09:11:49 +0300

    add the repositories logic

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Repositories } from 'src/app/repositories';
 import { ConfigService } from 'src/app/services/config.service';
 import { Users } from '../../users'
 
@@ -10,10 +11,18 @@ import { Users } from '../../users'
 export class ProfileComponent implements OnInit {
 
   user: Users = new Users("", "", "", "", 0, 0, 0)
+  repositories: Repositories[] = [];
+
+
   constructor(private request: ConfigService) {
-    this.request.getUser().subscribe(users =>{
-      console.log(users.avatar_url)
+    this.request.getUser().subscribe(users => {
+      console.log(users);
       this.user = users;
+    });
+
+    this.request.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repositories = repos;
     })
   }
   ngOnInit(): void {
